efb4dd4203d1e204842fec2bb0215a74d55248b0
Sophie-Ndobe <ndobes76@gmail.com>
2023-10-14 13:14:31 +0200

    Add state management to display the city value

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
+  const [city, setCity] = useState(" ");
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`${city}`);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
   return (
     <div className="Search">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-9">
-            <input type="search" placeholder="Enter a city name..." className="form-control" />
+            <input
+              type="search"
+              placeholder="Enter a city name..."
+              className="form-control"
+              onChange={updateCity}
+            />
           </div>
           <div className="col-3">
             <button className="btn btn-primary w-100">Search</button>
